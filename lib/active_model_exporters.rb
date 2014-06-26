7abed230ad2a722155c610f7219c895fc89e954d
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2014-06-26 18:33:50 -0500

    Adding csv render to action controller renderers

--- lib/active_model_exporters.rb
+++ lib/active_model_exporters.rb
@@ -2,3 +2,14 @@ require 'csv'
 require 'active_model'
 require 'active_model/exporter'
 require 'active_model/exporter/version'
+
+begin
+  require 'action_controller'
+  require 'action_controller/exportation'
+
+  ActiveSupport.on_load(:action_controller) do
+    include ::ActionController::Exportation
+  end
+rescue LoadError
+  # rails not installed, continuing
+end
